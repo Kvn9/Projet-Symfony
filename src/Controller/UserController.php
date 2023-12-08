c0ae9978d33f6328d6f1744c2cc27644ec2df5a2
Kvn9 <kevintata02@gmail.com>
2023-12-08 16:32:18 +0100

    Preview Final Version

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -19,6 +19,7 @@ class UserController extends AbstractController
     #[Route('/', name: 'app_user_index', methods: ['GET'])]
     public function index(UserRepository $userRepository): Response
     {
+        // Rend la page d'index des utilisateurs avec la liste de tous les utilisateurs
         return $this->render('user/index.html.twig', [
             'users' => $userRepository->findAll(),
         ]);
@@ -30,25 +31,34 @@ class UserController extends AbstractController
         Request $request,
         CartRepository $cartRepository
     ): Response {
+        // Récupère l'utilisateur actuellement connecté
         $user = $this->getUser();
         $allCarts = [];
 
+        // Vérifie si l'utilisateur est authentifié
         if ($user) {
+            // Crée un formulaire de modification pour l'utilisateur actuel
             $form = $this->createForm(UserType::class, $user);
             $form->handleRequest($request);
 
+            // Récupère les paniers de l'utilisateur
             $allCarts = $cartRepository->findBy(['user' => $user, 'state' => true]);
 
+            // Vérifie si le formulaire a été soumis et est valide
             if ($form->isSubmitted() && $form->isValid()) {
+                // Enregistre les modifications de l'utilisateur en base de données
                 $em->persist($user);
                 $em->flush();
 
+                // Ajoute un message flash pour informer de la modification réussie
                 $this->addFlash('success', 'Utilisateur modifié');
             }
         } else {
+            // Redirige vers la page de connexion si l'utilisateur n'est pas authentifié
             return $this->redirectToRoute('app_login');
         }
 
+        // Rend la page de modification de l'utilisateur avec les informations nécessaires
         return $this->render('user/edit.html.twig', [
             'user' => $user,
             'edit' => $form->createView(),
@@ -59,27 +69,33 @@ class UserController extends AbstractController
     #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
+        // Crée un nouvel utilisateur et un formulaire d'inscription
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
+        // Vérifie si le formulaire a été soumis et est valide
         if ($form->isSubmitted() && $form->isValid()) {
+            // Enregistre le nouvel utilisateur en base de données
             $entityManager->persist($user);
             $entityManager->flush();
 
+            // Redirige vers la page d'index des utilisateurs après l'enregistrement
             return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
         }
 
+        // Rend la page d'inscription avec le formulaire
         return $this->render('user/new.html.twig', [
             'user' => $user,
             'form' => $form,
         ]);
     }
 
-    //TODO check
+    // TODO check
     #[Route('/{id}', name: 'app_user_show', methods: ['GET'])]
     public function show(User $user): Response
     {
+        // Rend la page de détails d'un utilisateur
         return $this->render('user/show.html.twig', [
             'user' => $user,
         ]);
@@ -91,18 +107,27 @@ class UserController extends AbstractController
         User $user = null,
         EntityManagerInterface $em,
     ): Response {
+        // Vérifie si l'utilisateur existe
         if ($user == null) {
+            // Ajoute un message flash si l'utilisateur est introuvable
             $this->addFlash('danger', 'Utilisateur introuvable');
             return $this->redirectToRoute('app_user_index');
         }
+
+        // Vérifie si le token CSRF est valide
         if ($this->isCsrfTokenValid('delete' . $user->getId(), $request->request->get('_token'))) {
+            // Ajoute un message flash si le token CSRF est invalide
             $this->addFlash('danger', 'Token CSRF invalide');
+
+            // Supprime l'utilisateur de la base de données
             $em->remove($user);
             $em->flush();
 
+            // Ajoute un message flash pour informer de la suppression réussie
             $this->addFlash('success', 'Utilisateur supprimé');
         }
 
+        // Redirige vers la page d'index des utilisateurs
         return $this->redirectToRoute('app_user_index');
     }
 }
